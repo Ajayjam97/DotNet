d2d105cd44032eedc5c7d25d11ad404895159295
AjayJam97 <jamwal_ajay@hotmail.com>
2022-09-01 00:45:58 +0530

    Added Console application

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Answer().GetAnswer(40,2)}");
+        }
+    }
+}
