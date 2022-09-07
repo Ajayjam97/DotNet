6a7bf52596f25d62711a086ae58af2dc3002a094
AjayJam97 <jamwal_ajay@hotmail.com>
2022-09-07 23:54:28 +0530

    LLD

---
+++ ConsoleApp/Fundamentals/CustomDateTime.cs
@@ -0,0 +1,12 @@
+using System;
+using DateTime;
+
+
+namespace CustomDateTime{
+
+    class CustomDateTime{
+
+        DateTime dt=new DateTime();
+        Console.WriteLine(dt.ToString());
+    }
+}
