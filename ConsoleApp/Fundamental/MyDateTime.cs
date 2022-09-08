09fa069d312efe38e042a77459a0a78301ff2abe
AjayJam97 <jamwal_ajay@hotmail.com>
2022-09-09 02:22:47 +0530

    DateTime

--- ConsoleApp/Fundamental/MyDateTime.cs
+++ ConsoleApp/Fundamental/MyDateTime.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Threading;
 
 namespace ConsoleApp.Fundamental{
 
@@ -9,9 +11,24 @@ namespace ConsoleApp.Fundamental{
             DateTime dt=new DateTime();
             Console.WriteLine(dt);
             Console.WriteLine(DateTime.MaxValue);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.TryParse("09-09-2022 01:55:39", out dt));
 
+            CultureInfo currentculture= Thread.CurrentThread.CurrentCulture;
+            Console.WriteLine(currentculture);
+
+            string currentCulture = Thread.CurrentThread.CurrentCulture.DisplayName;  
+  
+            DateTime currentTime = DateTime.Now;  
+            
+            //// Getting DateTime based on culture.  
+            string dateInUSA = currentTime.ToString("D", new CultureInfo("en-US")); // USA  
+            string dateInFrench = currentTime.ToString("D", new CultureInfo("fr-FR")); // French 
+            Console.WriteLine(dateInUSA);
+            Console.WriteLine(dateInFrench); 
+                
         }
-        
+
     }
 
 }
